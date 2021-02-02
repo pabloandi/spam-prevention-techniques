4f60c999e3682b8480cabbe40baa966a9a14160b
Pablo Diaz <pabloandi@gmail.com>
2021-02-02 18:35:30 -0500

    the honeypot technique: I have a step forward, I created a middleware called check.spam

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,67 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Create Post
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form action="/posts" method="POST">
+                        @csrf
+                        <div class="mb-6">
+                            <label for="title" class="block mb-2 uppercase font-bold text-xs text-gray-700">Title</label>
+                            <input type="text" class="border border-gray-400 p-2 w-full" id="title" required name="title" autofocus>
+                            @error('title')
+                                <p class="text-red-500 text-xs mt-2">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                        </div>
+
+                        <div class="mb-6">
+                            <label for="body" class="block mb-2 uppercase font-bold text-xs text-gray-700">Body</label>
+                            <textarea class="border border-gray-400 p-2 w-full" id="body" required name="body"></textarea>
+                            @error('title')
+                                <p class="text-red-500 text-xs mt-2">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                        </div>
+
+                        <div style="display: none;">
+
+                            <div class="mb-6">
+                                <label for="myname" class="block mb-2 uppercase font-bold text-xs text-gray-700">My name</label>
+                                <input type="text" class="border border-gray-400 p-2 w-full" id="myname" name="myname">
+                                @error('myname')
+                                    <p class="text-red-500 text-xs mt-2">
+                                        {{ $message }}
+                                    </p>
+                                @enderror
+                            </div>
+
+                            <div class="mb-6" >
+                                <label for="mytime" class="block mb-2 uppercase font-bold text-xs text-gray-700">My time</label>
+                                <input type="text" class="border border-gray-400 p-2 w-full" id="mytime" name="mytime" value="{{ microtime(true) }}">
+                                @error('mytime')
+                                    <p class="text-red-500 text-xs mt-2">
+                                        {{ $message }}
+                                    </p>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="mb-6">
+                            <button class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Publish</button>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
